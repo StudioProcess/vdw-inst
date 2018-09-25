31bb0f98dfdb26b679bb5889fdfb79eced5c6bbe
RESL <moritz@resl.xyz>
2018-09-25 14:50:31 +0200

    Added color combinations

---
+++ components/colors.ts
@@ -0,0 +1,58 @@
+export default {
+    "00": {
+      bgColor: "#000000",
+      circleColor: "#ffffff"
+    },
+    "01": {
+      bgColor: "#ffffff",
+      circleColor: "#000000"
+    },
+    "02": {
+      bgColor: "#FF2600",
+      circleColor: "#000000"
+    },
+    "03": {
+      bgColor: "#FF2600",
+      circleColor: "#000000"
+    },
+    "04": {
+      bgColor: "#DCDCDC",
+      circleColor: "#000000"
+    },
+    "05": {
+      bgColor: "#FFDCDC",
+      circleColor: "#000000"
+    },
+    "06": {
+      bgColor: "#FFFFFB",
+      circleColor: "#0000FF"
+    },
+    "07": {
+      bgColor: "#1f294b",
+      circleColor: "#ECFFFF"
+    },
+    "08": {
+      bgColor: "#F1F1F2",
+      circleColor: "#000065"
+    },
+    "09": {
+      bgColor: "#F2004A",
+      circleColor: "#0054FF"
+    },
+    "10": {
+      bgColor: "#00004A",
+      circleColor: "#FF2600"
+    },
+    "11": {
+      bgColor: "#FF0000",
+      circleColor: "#FFFF52"
+    },
+    "12": {
+      bgColor: "#0031BC",
+      circleColor: "#FFF8BC"
+    },
+    "13": {
+      bgColor: "#000000",
+      circleColor: "#FF2600"
+    }
+  }
