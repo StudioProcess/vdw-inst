d189253f359c7c1efa4464e0688595aebde9b8e3
Martin Leopold <m@martinleopold.com>
2018-09-25 09:35:28 +0200

    Initial commit. (Copy from vdw-stage)

---
+++ components/events.ts
@@ -0,0 +1,142 @@
+export default {
+  "00": {
+    title: "A City\nFull of\nDesign",
+    type:  "Panel",
+    size:  200,
+  },
+  "01": {
+    title: "Fundus\nSophien-\nspital",
+    type:  "Panel",
+    size:  250,
+  },
+  "02": {
+    title: "Studio\nProtest",
+    type:  "Eröffnung",
+    size:  350,
+  },
+  "03": {
+    title: "Neue\nVisuelle\nInstrumente",
+    type:  "Talk",
+    size:  250,
+  },
+  "04": {
+    title: "Polish\nDesign\nand\nEntrepreneurship",
+    type:  "Panel",
+    size:  160,
+  },
+  "05": {
+    title: "Intra-\nTemporality",
+    type:  "Talk",
+    size:  200,
+  },
+  "06": {
+    title: "Joints\nand\nFittings",
+    type:  "Lecture",
+    size:  280,
+  },
+  "07": {
+    title: "Departure Talk 1:\nUrban\nFood\nStrategies",
+    type:  "Lecture",
+    size:  170,
+  },
+  "08": {
+    title: "Departure Talk 1:\nUrban\nFood\nStrategies",
+    type:  "Panel",
+    size:  170,
+  },
+  "09": {
+    title: "Departure Talk 2:\nDesigning\nthe\nExperience",
+    type:  "Panel",
+    size:  160,
+  },
+  "10": {
+    title: "Prophecy\nof the\nFallen",
+    type:  "Talk",
+    size:  230,
+  },
+  "11": {
+    title: "Teaser:\nDoing Nothing\nWith Ai",
+    type:  "Talk",
+    size:  210,
+  },
+  "12": {
+    title: "Atmove: Design\nals Treiber\nvon nachhaltigen\nInnovationen",
+    type:  "Talk",
+    size:  150,
+  },
+  "13": {
+    title: "Protestarchitektur\nvon Semper\nbis Occupy",
+    type:  "Lecture",
+    size:  150,
+  },
+  "14": {
+    title: "Designing\nRealities\nConference",
+    type:  "Conference",
+    size:  180,
+  },
+  "15": {
+    title: "DESIGNING REALITES:\nVirtual Reality\n&\nArchitecture",
+    type:  "Panel",
+    size:  120,
+  },
+  "16": {
+    title: "DESIGNING REALITES:\nArt\nUnframed",
+    type:  "Lecture",
+    size:  120,
+  },
+  "17": {
+    title: "DESIGNING REALITES:\nLevelling\nthe\nPlaying Field",
+    type:  "Keynote",
+    size:  120,
+  },
+  "18": {
+    title: "DESIGNING REALITES:\nVR\nBeyond Play",
+    type:  "Panel",
+    size:  120,
+  },
+  "19": {
+    title: "DESIGNING REALITES:\nXR zwischen\nAusstellungsraum\nund virtueller Realität",
+    type:  "Panel",
+    size:  120,
+  },
+  "20": {
+    title: "DESIGNING REALITES:\nDatabar",
+    type:  "Experience",
+    size:  120,
+  },
+  "21": {
+    title: "Sowieso Sozial!\nSocial Design\nim Dienst\nder Gesellschaft",
+    type:  "Panel",
+    size:  120,
+  },
+  "22": {
+    title: "Stadtarbeit-\nProjekte\n2018",
+    type:  "Präsentation",
+    size:  180,
+  },
+  "23": {
+    title: "Erste Bank\nMehrwert-Designpreis\n2018",
+    type:  "Preisverleihung",
+    size:  130,
+  },
+  "24": {
+    title: "Digitale Visionen\nund\nfeministische Perspektiven",
+    type:  "Panel",
+    size:  110,
+  },
+  "25": {
+    title: "The Role\nof Digitalization\nin Political Movements",
+    type:  "Panel",
+    size:  125,
+  },
+  "26": {
+    title: "Haltung\nzeigen!",
+    type:  "Panel",
+    size:  240,
+  },
+  "27": {
+    title: "Danke!\n \n \n ",
+    type:  "",
+    size:  280,
+  },
+};
