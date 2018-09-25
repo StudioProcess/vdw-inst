029343cfe0301e6ed9f869add08b7085ddf6c5c8
RESL <moritz@resl.xyz>
2018-09-25 15:55:37 +0200

    Added Parameter Presets

---
+++ components/presets.ts
@@ -0,0 +1,37 @@
+export default {
+  "00": {
+    divisionStep: 21,
+    cellDivide: 0.9,
+    cellFill: 0.9,
+    cellTwoDivisions: 0.9,
+    showPartial: false,
+  },
+  "01": {
+    divisionStep: 21,
+    cellDivide: 0.6,
+    cellFill: 1,
+    cellTwoDivisions: 1,
+    showPartial: false,
+  },
+  "02": {
+    divisionStep: 15,
+    cellDivide: 1,
+    cellFill: 1,
+    cellTwoDivisions: 1,
+    showPartial: false,
+  },
+  "03": {
+    divisionStep: 12,
+    cellDivide: 1,
+    cellFill: 0.9,
+    cellTwoDivisions: 1,
+    showPartial: false,
+  },
+  "04": {
+    divisionStep: 15,
+    cellDivide: 0.8,
+    cellFill: 1,
+    cellTwoDivisions: 0.02,
+    showPartial: false,
+  }
+}
