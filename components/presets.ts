0278728935f5800f2f8db4fe06d2d944770c103a
Martin Leopold <m@martinleopold.com>
2018-09-27 11:53:43 +0200

    reworked layout presets

--- components/presets.ts
+++ components/presets.ts
@@ -1,32 +1,62 @@
 export default {
-  "00": {
-    divisionStep: 21,
+  "02": {
+    divisionStep: 13,
+    cellDivide: 1,
+    cellFill: 1,
+    cellTwoDivisions: 0.99,
+    showPartial: false,
+  },
+  
+  "02_1": {
+    divisionStep: 11,
+    cellDivide: 1,
+    cellFill: 0.99,
+    cellTwoDivisions: 0.5,
+    showPartial: false,
+  },
+  
+  "02_2": {
+    divisionStep: 10,
+    cellDivide: 0.99,
+    cellFill: 0.8,
+    cellTwoDivisions: 0.99,
+    showPartial: false,
+  },
+  
+  "00_1": { // borg 1
+    divisionStep: 19,
     cellDivide: 0.9,
     cellFill: 0.9,
     cellTwoDivisions: 0.9,
     showPartial: false,
   },
-  "01": {
+  
+  "00_2": { // borg 2, very dense
     divisionStep: 21,
     cellDivide: 0.85,
     cellFill: 1,
     cellTwoDivisions: 1,
     showPartial: false,
   },
-  "02": {
+  
+  "00_3": { // borg 3
+    divisionStep: 23,
+    cellDivide: 0.66,
+    cellFill: 0.5,
+    cellTwoDivisions: 0.5,
+    showPartial: false,
+  },
+  
+   
+  "03": { // big and pretty ful
     divisionStep: 15,
-    cellDivide: 1,
-    cellFill: 1,
-    cellTwoDivisions: 1,
-    showPartial: false,
-  },
-  "03": {
-    divisionStep: 12,
-    cellDivide: 1,
+    cellDivide: 0.2,
     cellFill: 0.9,
-    cellTwoDivisions: 1,
+    cellTwoDivisions: 0.99,
     showPartial: false,
   },
+  
+  
   "04": {
     divisionStep: 15,
     cellDivide: 0.8,
@@ -34,6 +64,7 @@ export default {
     cellTwoDivisions: 0.02,
     showPartial: false,
   },
+  
   "05": {
     divisionStep: 19,
     cellDivide: 0.6,
@@ -41,6 +72,7 @@ export default {
     cellTwoDivisions: 1,
     showPartial: false,
   },
+  
   "06": {
     divisionStep: 19,
     cellDivide: 0.6,
@@ -48,52 +80,68 @@ export default {
     cellTwoDivisions: 1,
     showPartial: false,
   },
-  "07": {
+  "07": {  // wenige, rel. groß
     divisionStep: 6,
     cellDivide: 0.1,
     cellFill: 0.2,
-    cellTwoDivisions: 0.1,
+    cellTwoDivisions: 0.5,
     showPartial: false,
   },
-  "08": {
+  
+  
+  "08": { // wenige, dafuer bis ins kleinste
     divisionStep: 26,
     cellDivide: 0.1,
     cellFill: 0.2,
     cellTwoDivisions: 0.1,
     showPartial: false,
   },
-  "09": {
+  
+  "09": { // variante von 08
     divisionStep: 26,
     cellDivide: 0.1,
-    cellFill: 0.2,
-    cellTwoDivisions: 0,
+    cellFill: 0.3,
+    cellTwoDivisions: 0.5,
     showPartial: false,
   },
-  "10": {
+  
+  "10": { // starfield, regelmaessig
     divisionStep: 23,
-    cellDivide: 1,
+    cellDivide: 0.99,
     cellFill: 0.1,
     cellTwoDivisions: 0,
     showPartial: false,
   },
-  "11": {
+  
+  "11": { // starfield, untersch. groesse
     divisionStep: 15,
-    cellDivide: 1,
+    cellDivide: 0.9,
     cellFill: 0.1,
     cellTwoDivisions: 0.5,
     showPartial: false,
   },
-  "12": {
+  
+  
+  "12": { // with bueronardin (VDW)
     divisionStep: 6,
     cellDivide: 0.4,
     cellFill: 0.7,
     cellTwoDivisions: 0.5,
     showPartial: false,
   },
-  "13": {
+  
+  "13": { // bueronardin adjusted
     divisionStep: 5,
     cellDivide: 0.4,
     cellFill: 0.6,
+    cellTwoDivisions: 0.5,
+    showPartial: false,
+  },
+  
+  "14": { // our first default
+    divisionStep: 8,
+    cellDivide: 0.5,
+    cellFill: 0.66,
     cellTwoDivisions: 0.5,
     showPartial: false,
   }
